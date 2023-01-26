e8ee2d071ec88a27064ab65711092a6d53d8a247
gboye <afiz.lawal51@gmail.com>
2023-01-26 15:28:25 +0100

    addeed logic for numColums prop that was throwing error

--- screens/CategoriesScreen.tsx
+++ screens/CategoriesScreen.tsx
@@ -1,20 +1,22 @@
-import {FlatList} from "react-native" ;
+import {FlatList} from "react-native";
 import {CATEGORIES} from "../data/Dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
 
 type Iitem = {
-  item: {
-    title: string;
-    color: string;
-  }
+    item: {
+        title: string;
+        color: string;
+    }
 }
 
 function renderCategoryItem(itemData: Iitem) {
-  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
 }
+
 function CategoriesScreen() {
-  return <FlatList data={CATEGORIES} renderItem={renderCategoryItem} keyExtractor={(item) => item.id} />
+    const length = 4
+    return <FlatList data={CATEGORIES} renderItem={renderCategoryItem} keyExtractor={(item) => item.id} numColumns={length > 1 ? 2 : 1} />
 }
 
 export default CategoriesScreen
