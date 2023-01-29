fc8717e1f18f240575d1eb914464ed1d42b123a4
Gboyega Lawal <afiz.lawal51@gmail.com>
2023-01-29 22:41:41 +0100

    Added some reusable  components

--- screens/MealDetailScreen.tsx
+++ screens/MealDetailScreen.tsx
@@ -1,21 +1,91 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Pressable, Text, View } from "react-native";
+import {
+  StyleSheet,
+  ScrollView,
+  Text,
+  View,
+  Image,
+  Button,
+} from "react-native";
+import { useLayoutEffect } from "react";
 import { RootStackParamList } from "../types/navigationTypes";
-import { CATEGORIES } from "../data/Dummy-data";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import { MEALS } from "../data/Dummy-data";
+import List from "../components/List";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/Subtitle";
+import IconButton from "../components/IconButton";
 
 type Props = NativeStackScreenProps<RootStackParamList, "MealDetail">;
 function MealDetailScreen({ navigation, route }: Props) {
-  function pressHandler() {
-    console.log(route);
+  const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId)!;
+
+  function headerButtonPressHandler() {
+    console.log("Pressed!");
   }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        );
+      },
+    });
+  }, [navigation, headerButtonPressHandler]);
+
   return (
-    <View>
-      <Pressable onPress={pressHandler}>
-        <Text>This is the detail screen {route.params.mealId}</Text>
-      </Pressable>
-    </View>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+        style={undefined}
+      />
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
